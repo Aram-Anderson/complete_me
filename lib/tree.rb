feaf5521e3850b2400ad632738659a3aee61b50f
Aram Anderson <aram.j.anderson@gmail.com>
2017-07-08 08:39:00 -0600

    Work on insert method

--- lib/tree.rb
+++ lib/tree.rb
@@ -1,20 +1,20 @@
 require_relative 'node'
 
 class Tree
-  attr_reader :root
+  attr_reader :root, :insert
 
   def initialize
-    @root = {"a" => Node.new}
+    @root = Node.new
   end
 
   def insert(word, node = @root)
-    node.children.each do |k, v|
+    children.each do |k, v|
       if word.slice(0, 1) == k
         word.slice!(0, 1)
-        word.insert(word, node = node.children)
+        insert(word, node = node.children)
       else
         word.chars.each do |letter|
-          node.children[letter] = Node.new
+          children[letter] = Node.new
         end
       end
     end
