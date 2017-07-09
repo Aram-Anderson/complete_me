a82c4e8942990805f3c4ca9b3986b7ce4cf90304
Aram Anderson <aram.j.anderson@gmail.com>
2017-07-09 08:58:24 -0600

    Fix freq on root to stay at 0. Fix freq on other nodes to be accurate

--- lib/tree.rb
+++ lib/tree.rb
@@ -13,11 +13,14 @@ class Tree
   def insert(word, node = @root)
     if
       word == ""
+      @root.freq = 0
       node.valid_word = true
+      node.freq += 1
       @count += 1
     else
       saved_letter = word.slice!(0, 1)
       unless node.children[saved_letter].nil?
+        node.freq += 1
         insert(word, node.children[saved_letter])
       else
         node.children[saved_letter] = Node.new
