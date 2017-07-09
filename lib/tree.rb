49b814933febca4d6891a4208a3748d7cba59f37
Aram Anderson <aram.j.anderson@gmail.com>
2017-07-09 09:05:31 -0600

    Fix where freq gets incremented

--- lib/tree.rb
+++ lib/tree.rb
@@ -9,27 +9,27 @@ class Tree
     @count = 0
   end
 
-  # def insert(word)
   def insert(word, node = @root)
+    node.freq += 1
     if
       word == ""
       @root.freq = 0
       node.valid_word = true
-      node.freq += 1
       @count += 1
     else
       saved_letter = word.slice!(0, 1)
       unless node.children[saved_letter].nil?
-        node.freq += 1
         insert(word, node.children[saved_letter])
       else
         node.children[saved_letter] = Node.new
-        node.freq += 1
         insert(word, node.children[saved_letter])
       end
     end
   end
-  # end
+
+  def load(file)
+
+  end
 
 
 end
